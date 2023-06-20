dd90ae13a144b5324ba98d05467d1258f7011ff3
engageintellect <jc9361@gmail.com>
2023-06-20 00:25:27 -0700

    init2

--- app/page.tsx
+++ app/page.tsx
@@ -1,52 +1,61 @@
-'use client'
-import {
-  useState
-} from 'react'
+"use client";
+import { useState } from "react";
+import { ArrowPathIcon } from "@heroicons/react/24/solid";
 
 export default function Home() {
-  const [query, setQuery] = useState('')
-  const [result, setResult] = useState('')
-  const [loading, setLoading] = useState(false)
+  const [query, setQuery] = useState("");
+  const [result, setResult] = useState("");
+  const [loading, setLoading] = useState(false);
   async function createIndexAndEmbeddings() {
     try {
-      const result = await fetch('/api/setup', {
-        method: "POST"
-      })
-      const json = await result.json()
-      console.log('result: ', json)
+      const result = await fetch("/api/setup", {
+        method: "POST",
+      });
+      const json = await result.json();
+      console.log("result: ", json);
     } catch (err) {
-      console.log('err:', err)
+      console.log("err:", err);
     }
   }
   async function sendQuery() {
-    if (!query) return
-    setResult('')
-    setLoading(true)
+    if (!query) return;
+    setResult("");
+    setLoading(true);
     try {
-      const result = await fetch('/api/read', {
+      const result = await fetch("/api/read", {
         method: "POST",
-        body: JSON.stringify(query)
-      })
-      const json = await result.json()
-      setResult(json.data)
-      setLoading(false)
+        body: JSON.stringify(query),
+      });
+      const json = await result.json();
+      setResult(json.data);
+      setLoading(false);
     } catch (err) {
-      console.log('err:', err)
-      setLoading(false)
+      console.log("err:", err);
+      setLoading(false);
     }
   }
   return (
-    <main className="flex flex-col items-center justify-between p-24">
-      <input className='text-black px-2 py-1' onChange={e => setQuery(e.target.value)} />
-      <button className="px-7 py-1 rounded-2xl bg-white text-black mt-2 mb-2" onClick={sendQuery}>Ask AI</button>
-      {
-        loading && <p>Asking AI ...</p>
-      }
-      {
-        result && <p>{result}</p>
-      }
-      { /* consider removing this button from the UI once the embeddings are created ... */}
-      <button onClick={createIndexAndEmbeddings}>Create index and embeddings</button>
+    <main className="flex flex-col items-center justify-between p-24 max-w-5xl mx-auto">
+      <div className="flex gap-2">
+        <input
+          className="input border-primary w-full"
+          placeholder="search for anything..."
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button className="btn btn-primary" onClick={sendQuery}>
+          Submit
+        </button>
+      </div>
+
+      <div className="my-10">
+        {loading && <ArrowPathIcon className="h-10 w-10 animate-spin" />}
+        {result && <p>{result}</p>}
+      </div>
+
+      {/* consider removing this button from the UI once the embeddings are created ... */}
+      <button className="btn btn-outline" onClick={createIndexAndEmbeddings}>
+        Create index and embeddings
+      </button>
     </main>
-  )
+  );
 }
